541626db23191d1c369437fdf3a107d96eed45cb
vikashvikku <93948698+vikashvikku@users.noreply.github.com>
2025-10-31 23:12:17 +0530

    Refactor comments and improve static file serving

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-require('dotenv').config(); // Load .env variables locally
 
 const app = express();
 
-// Connect Database
+// Connect to MongoDB
 connectDB();
 
 // Init Middleware
@@ -17,15 +16,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Serve static assets in production
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
